0d5111569afda759d7c0d72929e443e4c3470559
normansrgn <normanmagic2004@gmail.com>
2024-05-12 09:59:30 +0300

    mobile adaptive

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,9 @@
 import mobileNav from './modules/mobile-nav.js';
 mobileNav();
 
+import burgerMenu from './modules/burgermenu.js';
+burgerMenu();
+
+
 // import isAvifWebp from 'avif-webp-checker';
 // isAvifWebp({ mode: 'webp' });
